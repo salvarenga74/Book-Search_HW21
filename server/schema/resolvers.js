c16971a11109bf3b0afa6efab87076724e800c39
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-10-18 18:14:28 -0500

    back end works after working with Dane. now trying to fix the routes to the pages

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
-const {} = require("../utils/auth");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
-
+const db = require("./config/connection");
+const routes = require("./routes");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schema/index");
-const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
@@ -25,6 +25,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.use(routes);
+
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
